f215a9597f29c4fae5251cd69098e757a7408d17
pushpanjali <pushpanjalipg700@gmail.com>
2025-03-21 16:12:01 +0530

    weather project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
+
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
+import { Grid, Card, Typography, Input } from '@mui/joy'
 function App() {
+  const [data, setData] = useState()
+  const [searchKey, setSearchKey] = useState()
+  const apiKey = 'b6033e5e'
+  const url = "http://www.omdbapi.com"
+  const fetchMovies = async () => {
+    fetch(`${url}?t=${searchKey}&apiKey=${apiKey}`).then((res) => {
+      if (!res.ok) {
+        throw new Error
+      }
+      return res.json()
+    }).then((data) => {
+      setData(data)
+    })
+    // if(searchKey.trim()==='') return
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container>
+      <Grid sm={12} sx={{ marginTop: "10px" }}>
+        <Input onChange={(e) => setSearchKey(e.target.value)} value={searchKey}
+          onKeyUp={(e) => {
+            if (e.key === 'Enter') {
+              fetchMovies()
+            }
+          }}></Input>
+      </Grid>
+      <Grid sm={4}>
+        <Card>
+          <Typography startDecorator="Title :">{data?.Title}</Typography>
+          <Typography startDecorator="Plot :">{data?.Plot}</Typography>
+          <Typography startDecorator="Poster :">
+            <img src={data?.Poster}></img>
+          </Typography>
+          <Typography startDecorator="Actors :">{data?.Actors}</Typography>
+        </Card>
+      </Grid>
+    </Grid>
   );
 }
 
